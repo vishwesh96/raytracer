f38ef6685bec044bbc9a97682d627f80d8853bc5
vishwesh96 <vishwesh1696@gmail.com>
2018-02-01 18:06:59 +0530

    Local illumination and reflection done, refraction has some pain

--- src/integrator.cpp
+++ src/integrator.cpp
@@ -4,8 +4,12 @@
 
 using namespace rt;
 
+int max_depth = 4;
 color_t whitted_integrator_t::radiance(const scene_t* _scn, ray_t& _ray, int& d) const
 {
+	if(d > max_depth) 
+		return color_t(0.0);
+	int depth = d+1;
 	bool found_intersection=false;
 	std::vector<object_t*>::const_iterator oit;
 	hit_t hit, minhit;
@@ -29,13 +33,42 @@ color_t whitted_integrator_t::radiance(const scene_t* _scn, ray_t& _ray, int& d)
 	color_t d_col(0.0);
 	if(found_intersection)
 	{
+		material_t * mat = minhit.first->get_material();
 		std::list<light_t*>::const_iterator lit;
 		for(lit=_scn->lits.begin(); lit!=_scn->lits.end(); lit++)
 		{
-			d_col += (*lit)->direct(hitpt, normal, minhit.first->get_material(), _scn);
+			d_col += (*lit)->direct(hitpt, normal, mat , _scn);
+		}
+
+		float epsilon = 1e-4;
+
+		Vector3f incident = _ray.direction.normalized();
+		float incident_dot_normal = incident.dot(normal);
+		
+		Vector3f transmitted;
+		ray_t transmitted_ray;
+		float nr;
+
+		if(incident_dot_normal < 0.0){		//entering the object
+			nr = 1.0/mat->get_eta();
+		} else {							//exiting the object
+			nr = mat->get_eta();
+			normal = -normal;
+									// printf("%s %f\n","hello",incident_dot_normal );
+		}
+
+		Vector3f reflected =  (incident - 2 * incident_dot_normal * normal).normalized();
+		ray_t reflected_ray(hitpt + epsilon * normal,reflected);
+
+		float D = 1 - (nr * nr *(1 - incident_dot_normal*incident_dot_normal));
+		if(D >= 0.0){				//normal refraction
+			transmitted = nr * incident - (nr * incident_dot_normal + sqrt(D)) * normal;
+			transmitted_ray = ray_t(hitpt - epsilon * normal ,transmitted);
+			d_col += mat->get_reflect() * radiance(_scn,reflected_ray,depth) + mat->get_transmit() * radiance(_scn,transmitted_ray,depth);
+		} else {					//TIR
+			d_col += radiance(_scn,reflected_ray,depth);
 		}
 	}
 	else d_col = _scn->img->get_bgcolor();
-
 	return d_col;
 }
--- src/light.cpp
+++ src/light.cpp
@@ -2,6 +2,9 @@
 
 using namespace rt;
 
+color_t vector3f_to_colour_t(const Vector3f);
+color_t multiply_color_t_vector3f(color_t col, Vector3f vec); 
+
 light_t::light_t() { }
 light_t::~light_t() { }
 
@@ -14,8 +17,32 @@ point_light_t::~point_light_t()
 
 color_t point_light_t::direct(const Vector3f& hitpt, const Vector3f& normal, const material_t* mat, const scene_t* scn) const
 {
-	color_t col(0.0,1.0,0.0);
-	return col;
+	color_t kd = mat -> get_diffuse();
+	color_t ks = mat -> get_specular();
+
+	Vector3f incident  = (pos - hitpt).normalized();
+	Vector3f reflected =  (2 * incident.dot(normal) * normal - incident).normalized();
+	Vector3f eye = scn -> cam -> get_eye();
+	Vector3f view = (eye - hitpt).normalized();
+
+	ray_t shadow_ray = ray_t(hitpt,incident);
+	bool in_shadow = false;
+	std::vector<object_t*> objects = scn -> objs;
+	for(unsigned int  i=0;i<objects.size();i++) {
+		hit_t result;
+		in_shadow = in_shadow || objects[i]->intersect(result,shadow_ray);
+	}
+
+	color_t ambient = vector3f_to_colour_t(ka * col);
+	color_t diffuse(0.0);
+	color_t specular(0.0);
+
+	if(!in_shadow) {
+		diffuse = multiply_color_t_vector3f(kd,col * incident.dot(normal)).clamp();
+		specular = multiply_color_t_vector3f(ks,col * pow(std::max(double(reflected.dot(view)),0.0),mat -> get_shininess()));
+	}
+	color_t total = ambient + diffuse + specular;
+	return total;
 }
 		
 
@@ -28,4 +55,12 @@ void point_light_t::print(std::ostream &stream) const
 	stream<<"Position: "<<pos.format(CommaInitFmt)<<std::endl;
 	stream<<"Color: "<<col.format(CommaInitFmt)<<std::endl;
 	stream<<"Ambient Coefficient: "<<ka<<std::endl<<std::endl;
+}
+
+color_t vector3f_to_colour_t(const Vector3f vec) {
+	return color_t(vec[0],vec[1],vec[2]);
+}
+
+color_t multiply_color_t_vector3f(color_t col, Vector3f vec) {
+	return color_t(col.r() * vec[0], col.g() * vec[1], col.b() * vec[2]);
 }
